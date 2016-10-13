859f3568bae9ff527189e57f02af42d7810d552b
Mark Adams <mark@markadams.me>
2016-10-12 22:24:40 -0500

    More work on inotify reloader

--- gunicorn/reloader.py
+++ gunicorn/reloader.py
@@ -63,6 +63,11 @@ except ImportError:
 if has_inotify:
 
     class InotifyReloader(threading.Thread):
+        valid_events = set((
+            'IN_CREATE', 'IN_DELETE', 'IN_DELETE_SELF',
+            'IN_MODIFY', 'IN_MOVE_SELF', 'IN_MOVED_FROM',
+            'IN_MOVED_TO'))
+        
         def __init__(self, extra_files=None, callback=None):
             super(InotifyReloader, self).__init__()
             self._callback = callback
@@ -76,9 +81,15 @@ if has_inotify:
                 return
 
             self._watcher.add_watch(dirname)
-            self._dirname.add(dirname)
+            self._dirs.add(dirname)
 
         def get_dirs(self):
+            fnames = [
+                module.__file__
+                for module in list(sys.modules.values())
+                if hasattr(module, '__file__')
+            ]
+
             fnames = [
                 os.path.dirname(re.sub('py[co]$', 'py', module.__file__))
                 for module in list(sys.modules.values())
@@ -89,16 +100,20 @@ if has_inotify:
 
         def run(self):
             self._dirs = self.get_dirs()
-
+            print(self._dirs)
             for dirname in self._dirs:
                 self._watcher.add_watch(dirname)
 
             for event in self._watcher.event_gen():
-                print(event)
                 if event is None:
                     continue
+                
+                types = event[1]
+                filename = event[3]
 
-                header, type_names, watch_path, filename = event
+                if len(set(types).intersection(self.valid_events)) == 0:
+                    continue
+
                 self._callback(filename)
 
 
